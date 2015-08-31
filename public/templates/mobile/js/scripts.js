23c12a0ee164d940dc0aff536c79b14b50734df6
Bob Ellis <bob.ellis@multco.us>
2015-08-31 16:28:22 -0700

    moving mobile pages to single page structure

--- public/templates/mobile/js/scripts.js
+++ public/templates/mobile/js/scripts.js
@@ -1,19 +1,5 @@
 var socket = io('http://52.26.186.75');
 
-$( document ).ready(function() {
-  var slideout = new Slideout({
-      'panel': document.getElementById('panel'),
-      'menu': document.getElementById('menu'),
-      'padding': 256,
-      'tolerance': 70
-    });
-
-    document.querySelector('.toggle-button').addEventListener('click', function() {
-    slideout.toggle();
-  });
-});
-
-
 socket.on('bridge data', function (data) {
   $("#bridges").text("");
   console.log("foo");
@@ -27,4 +13,37 @@ socket.on('bridge data', function (data) {
         ) + "<span class='bridge' data-role='content'>" + bridge + "</span>" + "</div>"
     );
   });
+});
+
+$( document ).ready(function() {
+  var slideout = new Slideout({
+      'panel': document.getElementById('panel'),
+      'menu': document.getElementById('menu'),
+      'padding': 256,
+      'tolerance': 70
+    });
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+      slideout.toggle();
+    });
+
+    $( "#menu-feed" ).click(function() {
+      $("#bridge-page").hide();
+      $("#feed-page").show();
+      $("#terms-page").hide();
+      slideout.toggle();
+    });
+
+    $( "#menu-home").click(function(){
+      $("#bridge-page").show();
+      $("#feed-page").hide();
+      $("#terms-page").hide();
+      slideout.toggle();
+    })
+
+    $( "#menu-terms").click(function(){
+      $("#bridge-page").hide();
+      $("#feed-page").hide();
+      $("#terms-page").show();
+      slideout.toggle();
+    })
 });
