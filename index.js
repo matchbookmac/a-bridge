9c6b89a18c8a548073ecc562afd3df40861a0892
Ian MacDonald <ian.macdonald@multco.us>
2015-09-10 14:39:40 -0700

    Change to new prod database

--- index.js
+++ index.js
@@ -7,22 +7,15 @@ var wlog            = require('winston');
 var User            = require('./models/index').User;
 var serverConfig    = require('./config/config');
 var sslConfig       = require('ssl-config')('intermediate');
-var options         = {
-  port: serverConfig.port
-  // tls: {
-  //   key: fs.readFileSync(path.join(__dirname + '/keys/server.key')),
-  //   cert: fs.readFileSync(path.join(__dirname + '/keys/server.crt')),
-  //   ca: fs.readFileSync(path.join(__dirname + '/keys/cs.crt'), 'utf8'),
-  //   requestCert: true,
-  //   rejectUnauthorized: false
-  // }
-};
+var options         = { port: serverConfig.port };
+
 var plugins = [
   { register: require('inert') },
   { register: require('vision') },
   { register: require('hapi-auth-bearer-token') },
   { register: require('lout') }
 ];
+
 var server = new Hapi.Server();
 server.connection(options);
 
