f005f94bbc2855da789a567b8bab3f07013733da
Ian MacDonald <ian.macdonald@multco.us>
2015-09-18 17:38:05 -0700

    Update receiving handlers to make new bridge model association

--- handlers/receive-actual-event.js
+++ handlers/receive-actual-event.js
@@ -5,66 +5,78 @@ var ActualEvent    = require('../models/index').actualEvent;
 var logger         = require('../config/logging');
 var bridgeStatuses = require('../config/config').bridges;
 var bridgeOpenings = require('../config/config').bridgeOpenings;
+var Bridge         = require('../models/index').bridge;
 var postBridgeMessage  = require('../modules/post-bridge-message');
 var handlePostResponse = require('../modules/handle-post-response');
 
 module .exports = function receiveActualEvent(request, reply) {
   var event = request.payload;
   var bridge = event.bridge.replace(/\'/g, "");
-  // Assemble current status for bridge
-  previousScheduledLift = bridgeStatuses[bridge] ? bridgeStatuses[bridge].scheduledLift : null;
-  bridgeStatuses[bridge] = {
-    status: event.status,
-    scheduledLift: event.status ? null : previousScheduledLift
-  };
-  bridgeStatuses.changed.bridge = bridge;
-  bridgeStatuses.changed.item = "status";
-  logger.info("%s %s at %s",
-    bridge,
-    bridgeStatuses[bridge].status ? 'up' : 'down',
-    event.timeStamp.toString()
-  );
-  var timeStamp  = strftime("%Y/%m/%d %H:%M:%S", event.timeStamp);
-  if (event.status){
-    reply("event up post received");
-    var actualEvent = {
+  Bridge.findOrCreate({
+    where: {
       name: bridge,
-      uptime: timeStamp
+      totalUpTime: {
+        $gte: 0
+      },
+      avgUpTime: {
+        $gte: 0
+      }
+    }
+  }).then(function (bridge) {
+    // Assemble current status for bridge
+    bridge = bridge[0];
+    previousScheduledLift = bridgeStatuses[bridge.name] ? bridgeStatuses[bridge.name].scheduledLift : null;
+    bridgeStatuses[bridge.name] = {
+      status: event.status,
+      scheduledLift: event.status ? null : previousScheduledLift
     };
-    //check to see if there are any unclosed bridge openings, if so then delete them and replace with this new bridge opening
-    for (i = 0; i < bridgeOpenings.length; i++){
-      if(bridgeOpenings[i].name === bridge){
-        bridgeOpenings.splice(i, 1);
+    bridgeStatuses.changed.bridge = bridge.name;
+    bridgeStatuses.changed.item = "status";
+    logger.info("%s %s at %s",
+      bridge.name,
+      bridgeStatuses[bridge.name].status ? 'up' : 'down',
+      event.timeStamp.toString()
+    );
+    var timeStamp  = strftime("%Y/%m/%d %H:%M:%S", event.timeStamp);
+    if (event.status){
+      reply("event up post received");
+      var actualEvent = {
+        name: bridge.name,
+        uptime: timeStamp
+      };
+      //check to see if there are any unclosed bridge openings, if so then delete them and replace with this new bridge opening
+      for (i = 0; i < bridgeOpenings.length; i++){
+        if(bridgeOpenings[i].name === bridge.name){
+          bridgeOpenings.splice(i, 1);
+        }
+      }
+      bridgeOpenings.push(actualEvent);
+    } else {
+      for (i = 0; i < bridgeOpenings.length; i++){
+        //check to see if there are any open bridge events that correspond with this close event
+        if (bridgeOpenings[i].name === bridge.name){
+          ActualEvent.create({
+            bridgeId: bridge.id,
+            up_time: bridgeOpenings[i].uptime,
+            down_time: timeStamp
+          }).then(successResponse)
+            .catch(errorResponse);
+          bridgeOpenings.splice(i, 1);
+        }
       }
     }
-    bridgeOpenings.push(actualEvent);
-  } else {
-    for (i = 0; i < bridgeOpenings.length; i++){
-      //check to see if there are any open bridge events that correspond with this close event
-      if (bridgeOpenings[i].name === bridge){
-// TODO: Change to match schema
-        ActualEvent.create({
-          bridge: bridge,
-          bridgeId: 123,
-          up_time: bridgeOpenings[i].uptime,
-          down_time: timeStamp
-        }).then(successResponse)
-          .catch(errorResponse);
-        bridgeOpenings.splice(i, 1);
-      }
-    }
-  }
-  postBridgeMessage(bridgeStatuses, null, function (err, res, status) {
-    handlePostResponse(status, bridgeStatuses, function (err, status) {
-      if (err) {
-        logger.error("Error posting\n%s\n%s\nStatus: %s",
-          util.inspect(bridgeStatuses),
-          util.inspect(err),
-          status
-        );
-      }
+    postBridgeMessage(bridgeStatuses, null, function (err, res, status) {
+      handlePostResponse(status, bridgeStatuses, function (err, status) {
+        if (err) {
+          logger.error("Error posting\n%s\n%s\nStatus: %s",
+            util.inspect(bridgeStatuses),
+            util.inspect(err),
+            status
+          );
+        }
+      });
     });
-  });
+  }).catch(errorResponse);
 
   function successResponse(event) {
     reply("event down post received");
--- handlers/receive-scheduled-event.js
+++ handlers/receive-scheduled-event.js
@@ -4,41 +4,53 @@ var ScheduledEvent = require('../models/index').scheduledEvent;
 var db             = require('../models/index');
 var logger         = require('../config/logging');
 var bridgeStatuses = require('../config/config').bridges;
+var Bridge         = require('../models/index').bridge;
 var postBridgeMessage  = require('../modules/post-bridge-message');
 var handlePostResponse = require('../modules/handle-post-response');
 
 module .exports = function receiveScheduledEvent(request, reply) {
   var event = request.payload;
   var bridge = event.bridge.replace(/\'/g, "");
-  delete event.bridge;
-  // Assemble current status for bridge
-  currentStatus = bridgeStatuses[bridge] ? bridgeStatuses[bridge].status : false;
-  bridgeStatuses[bridge] = {
-    status: currentStatus,
-    scheduledLift: event
-  };
-  event.bridge = bridge;
-// TODO: Change to match schema
-  event.bridgeId = 123;
-  bridgeStatuses.changed.bridge = bridge;
-  bridgeStatuses.changed.item = "scheduledLift";
-  logger.info("%s %s lift scheduled for %s at %s",
-    bridge,
-    bridgeStatuses[bridge].scheduledLift.type,
-    event.estimatedLiftTime.toString(),
-    event.requestTime.toString()
-  );
-  ScheduledEvent.create(event)
-                .then(function (event) {
-                  reply("schedule post received");
-                })
-                .catch(function (err) {
-                  reply(boom.badRequest("There was an error with your schedule post: " + err));
-                });
-  postBridgeMessage(bridgeStatuses, null, function (err, res, status) {
-    handlePostResponse(status, bridgeStatuses, function (err, status) {
-      if (err) logger.error("Error posting\n" + util.inspect(bridgeStatuses) + ":\n" + util.inspect(err) + "\n Status: " + status);
+  Bridge.findOrCreate({
+    where: {
+      name: bridge,
+      totalUpTime: {
+        $gte: 0
+      },
+      avgUpTime: {
+        $gte: 0
+      }
+    }
+  }).then(function (bridge) {
+    bridge = bridge[0];
+    // Assemble current status for bridge
+    currentStatus = bridgeStatuses[bridge.name] ? bridgeStatuses[bridge.name].status : false;
+    bridgeStatuses[bridge.name] = {
+      status: currentStatus,
+      scheduledLift: event
+    };
+    event.bridgeId = bridge.id;
+    bridgeStatuses.changed.bridge = bridge.name;
+    bridgeStatuses.changed.item = "scheduledLift";
+    logger.info("%s %s lift scheduled for %s at %s",
+      bridge.name,
+      bridgeStatuses[bridge.name].scheduledLift.type,
+      event.estimatedLiftTime.toString(),
+      event.requestTime.toString()
+    );
+    ScheduledEvent.create(event)
+                  .then(function (event) {
+                    reply("schedule post received");
+                  })
+                  .catch(function (err) {
+                    reply(boom.badRequest("There was an error with your schedule post: " + err));
+                  });
+    postBridgeMessage(bridgeStatuses, null, function (err, res, status) {
+      handlePostResponse(status, bridgeStatuses, function (err, status) {
+        if (err) logger.error("Error posting\n" + util.inspect(bridgeStatuses) + ":\n" + util.inspect(err) + "\n Status: " + status);
+      });
     });
+  }).catch(function (err) {
+    reply(boom.badRequest("There was an error with your schedule post: " + err));
   });
-
 };
