df13bc97277f7146bf67508b297e9d25c7784074
Ian MacDonald <ian.macdonald@multco.us>
2015-09-18 19:48:48 -0700

    Add module to calculate total uptime for bridges

---
+++ test/migrate-bridges-total-up-time.js
@@ -0,0 +1,81 @@
+var ActualEvent = require('../models/index').actualEvent;
+var Bridge      = require('../models/index').bridge;
+var Promise     = require("bluebird");
+
+Bridge.findAll().then(function (bridges) {
+  bridges.forEach(function (bridge) {
+    var totalUpTime = 0;
+    Promise.all([
+      ActualEvent.findAll({
+        where: {
+          bridgeId: bridge.id
+        }
+      }),
+      ActualEvent.count({
+        where: {
+          bridgeId: bridge.id
+        }
+      })
+    ]).then(function (results) {
+      var events = results[0];
+      var count = results[1];
+      events.forEach(function (event) {
+        totalUpTime = totalUpTime + (new Date(event.downTime) - new Date(event.upTime));
+      });
+      bridge.update({
+        totalUpTime: totalUpTime,
+        avgUpTime: (totalUpTime/count)
+      }).then(function (success) {
+        return;
+      })
+      .catch(function (err) {
+        console.log(err);
+        return;
+      });
+    }).catch(function (err) {
+      console.log(err);
+      return;
+    });
+  });
+}).catch(function (err) {
+  console.log(err);
+  return;
+});
+// Bridge.findAll().then(function (bridges) {
+//   bridges.forEach(function (bridge) {
+//     var totalUpTime = 0;
+//     Promise.all([
+//       ActualEvent.findAll({
+//         where: {
+//           bridgeId: bridge.id
+//         }
+//       }),
+//       ActualEvent.count({
+//         where: {
+//           bridgeId: bridge.id
+//         }
+//       })
+//     ]).then(function (results) {
+//       var events = results[0];
+//       var count = results[1];
+//       events.forEach(function (event) {
+//         totalUpTime = totalUpTime + (new Date(event.downTime) - new Date(event.upTime));
+//       });
+// console.log(totalUpTime);
+//       bridge.update({
+//         totalUpTime: totalUpTime,
+//         avgUpTime: (totalUpTime/count)
+//       });
+//     }).catch(function (err) {
+//       console.log(err);
+//       throw err;
+//     });
+//   });
+// });
+// Bridge.findOne({ where: { name: 'hawthorne' } }).then(function (bridge) {
+//   bridge.getActualEvents().then(function (events) {
+//     events.forEach(function (event) {
+//       event.update({ downTime: new Date() });
+//     });
+//   });
+// });
--- test/test.js
+++
@@ -1,7 +0,0 @@
-var Bridge = require('../models/index').bridge;
-
-Bridge.findOne({ where: { name: 'hawthorne' } }).then(function (bridge) {
-  bridge.getScheduledEvents().then(function (events) {
-    console.log(events);
-  });
-});
