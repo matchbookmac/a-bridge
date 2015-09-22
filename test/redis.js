512ed820c8f30b220d197b2ced6c0c6357bf15cf
Ian MacDonald <ian.macdonald@multco.us>
2015-09-22 16:06:37 -0700

    Update seed script to delete ibridge token, don't need it

--- test/redis.js
+++ test/redis.js
@@ -3,73 +3,77 @@ var crypto = require('crypto');
 var bcrypt = require('bcrypt');
 var serverConfig = require('../config/config');
 
-var client = redis.createClient(serverConfig.redis.port, serverConfig.redis.host);
-client.auth(serverConfig.redis.password, function (err, res) {
-  if (err) return console.log('Problem connecting to redis: '+ err);
-  console.log('Connected to Redis at: '+ client.address);
-});
+var redisStore = redis.createClient(serverConfig.redis.port, serverConfig.redis.host);
+if (serverConfig.redis.password) {
+  redisStore.auth(serverConfig.redis.password, function (err, res) {
+    if (err) return logger.error('Problem connecting to redis: '+ err);
+    logger.info('Connected to Redis at: '+ redisStore.address);
+  });
+} else {
+  logger.info('Connected to Redis at: '+ redisStore.address);
+}
 client.on("error", function (err) {
     console.log("Error " + err);
 });
-crypto.randomBytes(16, function(ex, buf) {
-  var token = buf.toString('hex');
-  var hashToken;
-  console.log(token);
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(token, salt, function(err, hash) {
-      if (err) return console.log(err);
-      hashToken = hash;
-      console.log(hashToken);
-      client.set('lbridge@co.multnomah.or.us', hashToken, function (err, res) {
-        console.log(err);
-        console.log(res);
-      });
-    });
-  });
-});
-crypto.randomBytes(16, function(ex, buf) {
-  var token = buf.toString('hex');
-  var hashToken;
-  console.log(token);
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(token, salt, function(err, hash) {
-      if (err) return console.log(err);
-      hashToken = hash;
-      console.log(hashToken);
-      client.set('abridge@api.multco.us', hashToken, function (err, res) {
-        console.log(err);
-        console.log(res);
-      });
-    });
-  });
-});
-crypto.randomBytes(16, function(ex, buf) {
-  var token = buf.toString('hex');
-  var hashToken;
-  console.log(token);
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(token, salt, function(err, hash) {
-      if (err) return console.log(err);
-      hashToken = hash;
-      console.log(hashToken);
-      client.set('ibridge@api.multco.us', hashToken, function (err, res) {
-        console.log(err);
-        console.log(res);
-      });
-    });
-  });
-});
+// crypto.randomBytes(16, function(ex, buf) {
+//   var token = buf.toString('hex');
+//   var hashToken;
+//   console.log(token);
+//   bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash(token, salt, function(err, hash) {
+//       if (err) return console.log(err);
+//       hashToken = hash;
+//       console.log(hashToken);
+//       client.set('lbridge@co.multnomah.or.us', hashToken, function (err, res) {
+//         console.log(err);
+//         console.log(res);
+//       });
+//     });
+//   });
+// });
+// crypto.randomBytes(16, function(ex, buf) {
+//   var token = buf.toString('hex');
+//   var hashToken;
+//   console.log(token);
+//   bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash(token, salt, function(err, hash) {
+//       if (err) return console.log(err);
+//       hashToken = hash;
+//       console.log(hashToken);
+//       client.set('abridge@api.multco.us', hashToken, function (err, res) {
+//         console.log(err);
+//         console.log(res);
+//       });
+//     });
+//   });
+// });
+// crypto.randomBytes(16, function(ex, buf) {
+//   var token = buf.toString('hex');
+//   var hashToken;
+//   console.log(token);
+//   bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash(token, salt, function(err, hash) {
+//       if (err) return console.log(err);
+//       hashToken = hash;
+//       console.log(hashToken);
+//       client.set('ibridge@api.multco.us', hashToken, function (err, res) {
+//         console.log(err);
+//         console.log(res);
+//       });
+//     });
+//   });
+// });
 
 // client.get('thing', function (err, res) {
 //   console.log(err);
 //   console.log(res);
 // });
 //
-// client.del('1234', function (err, res) {
-//   console.log(err);
-//   console.log(res);
-//   client.quit();
-// });
+client.del('ibridge@api.multco.us', function (err, res) {
+  console.log(err);
+  console.log(res);
+  client.quit();
+});
 //
 // client.get('thing', function (err, res) {
 //   console.log(err);
