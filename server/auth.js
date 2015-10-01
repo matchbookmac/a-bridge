a9744c7ad4b20dfb41525573eafba911db5dc316
Ian on MacBook Pro <imacd20@gmail.com>
2015-09-30 21:29:17 -0700

    Make user auth logging more explicit

--- server/auth.js
+++ server/auth.js
@@ -12,21 +12,22 @@ exports = module.exports = function (logger, redisStore) {
         redisStore.get(email, function (err, hashToken) {
           if (err) errorResponse(err);
           // Compare the stored hash with the token provided
-          bcrypt.compare(secret, hashToken, function(err, res) {
-            if (err) errorResponse(err);
-            if (res) {
-              logger.info('User: '+ email +' has authenticated');
-              return callback(null, true, { user: email, token: secret });
-            } else {
-              logger.warn('User: '+ email +' failed authentication');
-              return callback(null, false, { user: email, token: secret });
-            }
-          });
+          if (hashToken) {
+            bcrypt.compare(secret, hashToken, function(err, res) {
+              if (err) logger.error(err);
+              if (res) {
+                logger.info('User: '+ email +' has authenticated');
+                return callback(null, true, { user: email, token: secret });
+              } else {
+                logger.warn('User: '+ email +' failed authentication');
+                return callback(null, false, { user: email, token: secret });
+              }
+            });
+          } else {
+            logger.warn('User: '+ email +' does not exist');
+            return callback(null, false, { user: email, token: secret });
+          }
         });
-        function errorResponse(err) {
-          logger.error(err);
-          return callback(null, false, { user: null, token: secret });
-        }
       }
     });
   }
